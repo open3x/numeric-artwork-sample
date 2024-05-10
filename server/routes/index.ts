4386858e9d6d5a6297723f07b2cc91dbf6b21971
oji3 <oji3xx@gmail.com>
2024-05-10 22:51:58 +0800

    fix

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,29 +1,49 @@
-import Jimp from "jimp";
+import { GlobalFonts, createCanvas, loadImage } from "@napi-rs/canvas";
 
 export default eventHandler(async (event) => {
-  //
-  const query = getRequestURL(event).searchParams;
-  const address = query.get("address");
-  const value = query.get("value");
+  const { address, value } = getQuery(event);
+  if (!address || !value) {
+    setResponseStatus(event, 400);
+    return { error: "address and value are required" };
+  }
 
-  //
-  const image = await Jimp.read("./public/phi.png");
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-  image.print(
-      font,
-      0,
-      0,
-      {
-        text: address + " " + value,
-        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
-      },
-      image.bitmap.width,
-      image.bitmap.height
-  );
+  const [canvasW, canvasH] = [512, 512];
+  const canvas = createCanvas(canvasW, canvasH);
+  const ctx = canvas.getContext('2d');
+  {
+    const [rectW, rectH] = [448, 200];
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 4;
+    const rectX = (canvasW - rectW) / 2;
+    const rectY = (canvasH - rectH) / 2;
+    ctx.fillRect(rectX, rectY, rectW, rectH);
+    ctx.strokeRect(rectX, rectY, rectW, rectH);
 
-  //
-  const buf = await image.getBufferAsync(Jimp.MIME_PNG);
-  setHeaders(event, { 'Content-Type': 'image/png' });
+    const host = event.node.req.headers.host;
+    const base = (process.env.NODE_ENV === "development" ? "http://" : "https://") + host;
+    const image = await loadImage(new URL("/p.png", base));
+    const [imageW, imageH] = [96, 96];
+    const imageX = rectX + 32;
+    const imageY = rectY + rectH / 2 - imageH / 2;
+    ctx.drawImage(image, imageX, imageY, imageW, imageH);
+
+    const font = await useStorage("assets:server").getItemRaw("Roboto-Bold.ttf");
+    GlobalFonts.register(font, "Roboto-Bold");
+    const text = value.toLocaleString();
+    ctx.font = "96px Roboto-Bold";
+    ctx.lineWidth = 6;
+    const metrics = ctx.measureText(text);
+    const textW = metrics.width;
+    const textH = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+    const textX = imageX + imageW / 2 + (rectW - imageW / 2 - textW) / 2;
+    const textY = rectY + (rectH / 2) - (textH / 2) + metrics.actualBoundingBoxAscent;
+    ctx.fillStyle = "black";
+    ctx.fillText(text, textX, textY);
+  }
+
+  const buf = canvas.toBuffer("image/png");
+  setHeaders(event, { "Content-Type": "image/png" });
+  setResponseStatus(event, 200);
   return buf;
 });
